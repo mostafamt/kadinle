2c96e40bb1b0ec6f002f15e48ad3be64ad2c1ea4
myosry91 <m.yosry91@yahoo.com>
2024-07-09 18:36:43 +0300

    about us local img2

--- app/[locale]/components/home/AboutUs.js
+++ app/[locale]/components/home/AboutUs.js
@@ -7,6 +7,7 @@ import { Pagination, Navigation, Autoplay } from "swiper";
 import Image from "next/image";
 
 const AboutUs = ({ translations }) => {
+  // local data
   const aboutUsObj = [
     {
       id: 1,
